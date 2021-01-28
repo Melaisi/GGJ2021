a30c2e83dbcf5c3c4986bdd7852621ef3e41a8d4
OsamaArab <osama.f.arab@gmail.com>
2021-01-28 19:53:19 +0300

    More of osama's
    Timer, Movement, Flash.

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    //public float RotationSpeed = 50f;
+    public float speedH = 2.0f;
+    public float speedV = 2.0f;
+
+    private float yaw = 0.0f;
+    private float pitch = 0.0f;
+
+    void Update()
+    {
+        /* 
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Rotate(Vector3.left * RotationSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Rotate(Vector3.right * RotationSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.down * RotationSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(Vector3.up * RotationSpeed * Time.deltaTime);
+        }
+        */
+
+        yaw += speedH * Input.GetAxis("Mouse X");
+        pitch -= speedV * Input.GetAxis("Mouse Y");
+        transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+    }
+}
--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -1,18 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class Timer : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float currentTime = 0f;
+    public float startingTime = 30;
+  
+    [SerializeField] Text countdownText;
     void Start()
     {
-        
+        currentTime = startingTime;
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        currentTime -= 1 * Time.deltaTime;
+        countdownText.text = currentTime.ToString("0");
+
+        if (currentTime <= 0)
+        {
+            currentTime = 0;
+        }
+        if (currentTime <= 10)
+        {
+            countdownText.color = Color.red;
+        }
     }
 }
