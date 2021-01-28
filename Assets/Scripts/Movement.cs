598b7ad8dd8a8cdb9dd588753d11b215c732e637
OsamaArab <osama.f.arab@gmail.com>
2021-01-28 20:37:53 +0300

    fixing movement

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,23 +4,22 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    //public float RotationSpeed = 50f;
+    public float RotationSpeed = 50f;
     public float speedH = 2.0f;
     public float speedV = 2.0f;
 
     private float yaw = 0.0f;
     private float pitch = 0.0f;
     //Rotation Sensitivity
-    float RotationSensitivity = 500.0f; // help controll how far you can see
-    float minAngle = -180.0f;
-    float maxAngle = 180.0f;
+    //float RotationSensitivity = 500.0f; // help controll how far you can see
+    //float minAngle = -180.0f;
+    //float maxAngle = 180.0f;
 
     //Rotation Value
-    float yRotate = 0.0f;
-    float xRotate = 0.0f;
+    //float yRotate = 0.0f;
+    //float xRotate = 0.0f;
     void Update()
     {
-        /* 
         if (Input.GetKey(KeyCode.W))
         {
             transform.Rotate(Vector3.left * RotationSpeed * Time.deltaTime);
@@ -37,22 +36,22 @@ public class Movement : MonoBehaviour
         {
             transform.Rotate(Vector3.up * RotationSpeed * Time.deltaTime);
         }
-        */
+
 
         //yaw += speedH * Input.GetAxis("Mouse X");
-        //pitch -= speedV * Input.GetAxis("Mouse Y");
-        //transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+        pitch -= speedV * Input.GetAxis("Mouse Y");
+        transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
 
         // Testing a solution to rotate camera based on : https://answers.unity.com/questions/762475/limit-range-camera-rotation.html 
         //Rotate Y view
         
-        yRotate += Input.GetAxis("Mouse Y") * RotationSensitivity * Time.deltaTime;
-        xRotate += Input.GetAxis("Mouse X") * RotationSensitivity * Time.deltaTime;
+        //yRotate += Input.GetAxis("Mouse Y") * RotationSensitivity * Time.deltaTime;
+        //xRotate += Input.GetAxis("Mouse X") * RotationSensitivity * Time.deltaTime;
 
-        yRotate = Mathf.Clamp(yRotate, minAngle, maxAngle);
-        xRotate = Mathf.Clamp(xRotate, minAngle, maxAngle);
+        //yRotate = Mathf.Clamp(yRotate, minAngle, maxAngle);
+        //xRotate = Mathf.Clamp(xRotate, minAngle, maxAngle);
 
-        transform.eulerAngles = new Vector3(yRotate, xRotate, 0.0f);
+        //transform.eulerAngles = new Vector3(yRotate, xRotate, 0.0f);
 
     }
 }
