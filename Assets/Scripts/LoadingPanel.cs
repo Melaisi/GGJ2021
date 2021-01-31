c5229b84524f454736f6679ba65917553f847c7b
OsamaArab <osama.f.arab@gmail.com>
2021-01-31 14:52:28 +0300

    Final Scene
    Loading screen,

---
+++ Assets/Scripts/LoadingPanel.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoadingPanel : MonoBehaviour
+{
+    public GameObject loadingScreen;
+    public Slider slider;
+    public Text progressText;
+
+    public void LoadLevel(int sceneIndex)
+    {
+        StartCoroutine(LoadAsynchronously(sceneIndex));
+        loadingScreen.SetActive(true);
+    }
+    IEnumerator LoadAsynchronously(int sceneIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / .9f);
+            slider.value = progress;
+            progressText.text = progress * 100f + "%";
+            yield return null;
+            Debug.Log(progress);
+        }
+    }
+}
